a426bbfb3c20713ebade51ef9a12990c1a276818
basant <animeindia.96@gmail.com>
2025-06-09 19:54:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
+
+import os
 from handlers.uno import uno_join, uno_start, uno_play, uno_hand, uno_status
+TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    raise ValueError("⚠️ TOKEN not set in environment variables!")
 
 dispatcher.add_handler(CommandHandler("uno_join", uno_join))
 dispatcher.add_handler(CommandHandler("uno_start", uno_start))
